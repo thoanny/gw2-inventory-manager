08e1b5fd4bd56ead30222454231894c883e825fb
Anthony Destenay <anthony.destenay@gmail.com>
2022-05-09 14:36:13 +0200

    Added tip for items: 46731, 46733, 46735, 49262, 66953, 66985, 66992, 66988, 73163, 74268, 82518, 84335, 89140

--- tips.js
+++ tips.js
@@ -5,11 +5,31 @@ let tips = {
 
     45001: 'Si tu as déjà débloqué cette miniature, tu peux la détruire.',
 
+    46731: 'Utile pour plusieurs recettes. Si tu en as trop, fabrique quelques [Briques de pierre de sang](https://wiki-fr.guildwars2.com/wiki/Brique_de_pierre_de_sang) ou utilise-les avec les [convertisseurs de matériaux](https://www.lebusmagique.fr/pages/guides/guide-lvl-80/les-convertisseurs-de-materiaux.html).',
+    46733: 'Utile pour plusieurs recettes. Si tu en as trop, fabrique quelques [Lingots de dragonite](https://wiki-fr.guildwars2.com/wiki/Lingot_de_dragonite) ou utilise-les avec les [convertisseurs de matériaux](https://www.lebusmagique.fr/pages/guides/guide-lvl-80/les-convertisseurs-de-materiaux.html).',
+    46735: 'Utile pour plusieurs recettes. Si tu en as trop, fabrique quelques [Étoiles empyréennes](https://wiki-fr.guildwars2.com/wiki/Etoile_empyr%C3%A9enne) ou utilise-les avec les [convertisseurs de matériaux](https://www.lebusmagique.fr/pages/guides/guide-lvl-80/les-convertisseurs-de-materiaux.html).',
+
+    49262: 'Garde-le si tu souhaites fabriquer l\'[Espadon fractal dorée](https://wiki-fr.guildwars2.com/wiki/Espadon_fractal_dor%C3%A9) pour la collection [Spécialiste des fractales dorées](https://www.lebusmagique.fr/pages/succes/les-collections/collections-rares/collection-rare-specialiste-des-fractales-dorees.html). Jete-le si tu as déjà validé l\'apparence dorée.',
+
     66909: 'Si tu as terminé [Mawdrey](https://lebusmagique.fr/pages/mises-a-jour-passees/col-aride/objet-de-dos-maudrey-et-maudrey-ii.html), tu peux jeter la graine.',
+    66953: 'Ouvre le cadeau pour récupérer les récompenses.',
+    66985: 'Si tu as terminé [Mawdrey](https://lebusmagique.fr/pages/mises-a-jour-passees/col-aride/objet-de-dos-maudrey-et-maudrey-ii.html), tu peux jeter l\'orbe.',
+    66992: 'Si tu as terminé [Mawdrey](https://lebusmagique.fr/pages/mises-a-jour-passees/col-aride/objet-de-dos-maudrey-et-maudrey-ii.html), tu peux jeter cet objet.',
+    66988: 'Si tu as terminé [Mawdrey](https://lebusmagique.fr/pages/mises-a-jour-passees/col-aride/objet-de-dos-maudrey-et-maudrey-ii.html), tu peux jeter cet objet.',
 
     70026: 'Détruis-le ou garde-le pour le jeter à la Forge mystique.',
 
+    73163: 'À recycler si ses statistiques ne t\'intéressent pas...',
+
+    74268: 'À utiliser à la Forge Mystique pour terminer la collection [Gros Tom](https://www.lebusmagique.fr/pages/succes/les-collections/collections-basiques/collection-basique-gros-tom.html).',
+
+    82518: 'Consomme la lettre pour obtenir 1&nbsp;000 points de karma.',
+
+    84335: 'Utile pour la [Chasse aux trésors enfouis](https://www.lebusmagique.fr/pages/gw2-pof/hautes-terres-du-desert/metapof-tresor-enfoui.html).',
+
     87517: 'Consomme ces clés en réalisant le succès [Maître de la plongée](https://lebusmagique.fr/pages/succes/general/chasseur-de-tresor-englouti.html) (10 clés). Quelques pièces d\'or à la... clé&nbsp;! 😉',
+
+    89140: 'Transforme-les en [Tas de cristaux luisants](https://wiki-fr.guildwars2.com/wiki/Tas_de_cristaux_luisants) pour libérer de la place (10 particules =&nbsp;1&nbsp;tas).',
 
     92209: 'Utilise-le sur un personnage que tu joues beaucoup pour libérer un emplacement d\'inventaire.',
     92948: 'Tu peux le détruire.'
