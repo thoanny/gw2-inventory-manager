cea7055576d27c0bcfbb6d8a67cac154a9a797cc
Anthony Destenay <anthony.destenay@gmail.com>
2022-05-11 09:14:30 +0200

    Tips for 24531, 37087, 47083, 48717, 49218, 64562, 66961, 70229, 75036, 79351, 82589, 83008, 84731, 85016 and 92317

--- tips.js
+++ tips.js
@@ -3,34 +3,58 @@ let tips = {
 
     20030: 'Tu peux détruire cet objet. Il se rachète à Sarettokk à l\'Arche du Lion.',
 
+    24531: 'À vendre au comptoir ou à un marchand, en fonction des prix, des offres et des demandes.',
+
+    37087: 'À infuser et/ou passer en affinité en fonction de tes ressources, avant de recycler si les stats ne t\'intéressent pas.',
+
     45001: 'Si tu as déjà débloqué cette miniature, tu peux la détruire.',
 
     46731: 'Utile pour plusieurs recettes. Si tu en as trop, fabrique quelques [Briques de pierre de sang](https://wiki-fr.guildwars2.com/wiki/Brique_de_pierre_de_sang) ou utilise-les avec les [convertisseurs de matériaux](https://www.lebusmagique.fr/pages/guides/guide-lvl-80/les-convertisseurs-de-materiaux.html).',
     46733: 'Utile pour plusieurs recettes. Si tu en as trop, fabrique quelques [Lingots de dragonite](https://wiki-fr.guildwars2.com/wiki/Lingot_de_dragonite) ou utilise-les avec les [convertisseurs de matériaux](https://www.lebusmagique.fr/pages/guides/guide-lvl-80/les-convertisseurs-de-materiaux.html).',
     46735: 'Utile pour plusieurs recettes. Si tu en as trop, fabrique quelques [Étoiles empyréennes](https://wiki-fr.guildwars2.com/wiki/Etoile_empyr%C3%A9enne) ou utilise-les avec les [convertisseurs de matériaux](https://www.lebusmagique.fr/pages/guides/guide-lvl-80/les-convertisseurs-de-materiaux.html).',
 
+    47083: 'Si tu l\'as lié à l\'âme d\'un personnage tu ne peux plus le vendre, mais toujours le jeter à la Forge mystique ou le recycler.',
+
+    48717: '5 morceaux =&nbsp;1 clé. Ces clés ouvrent des coffres dans le chemin de l\'éther, dans le donjon "Tonnelle du Crépuscule". À jeter si tu ne comptes pas faire de donjons.',
+
+    49218: 'Si tu as terminé les deux collections d\'armes [Spécialiste des fractales](https://www.lebusmagique.fr/pages/succes/les-collections/collections-rares/collection-rare-specialiste-des-fractales.html) et [Spécialiste des fractales dorées](https://www.lebusmagique.fr/pages/succes/les-collections/collections-rares/collection-rare-specialiste-des-fractales-dorees.html) (utile pour obtenir une apparence précise), tu peux jeter.',
     49262: 'Garde-le si tu souhaites fabriquer l\'[Espadon fractal dorée](https://wiki-fr.guildwars2.com/wiki/Espadon_fractal_dor%C3%A9) pour la collection [Spécialiste des fractales dorées](https://www.lebusmagique.fr/pages/succes/les-collections/collections-rares/collection-rare-specialiste-des-fractales-dorees.html). Jete-le si tu as déjà validé l\'apparence dorée.',
+
+    64562: 'Si tu n\'en as pas besoin, tu peux la jeter à la Forge mystique.',
 
     66909: 'Si tu as terminé [Mawdrey](https://lebusmagique.fr/pages/mises-a-jour-passees/col-aride/objet-de-dos-maudrey-et-maudrey-ii.html), tu peux jeter la graine.',
     66953: 'Ouvre le cadeau pour récupérer les récompenses.',
+    66961: 'Donne un bonus de stats, de karma, de découverte de magie et d\'expérience, mais il ne faut pas déjà être sous l\'effet d\'une nourriture.',
     66985: 'Si tu as terminé [Mawdrey](https://lebusmagique.fr/pages/mises-a-jour-passees/col-aride/objet-de-dos-maudrey-et-maudrey-ii.html), tu peux jeter l\'orbe.',
     66992: 'Si tu as terminé [Mawdrey](https://lebusmagique.fr/pages/mises-a-jour-passees/col-aride/objet-de-dos-maudrey-et-maudrey-ii.html), tu peux jeter cet objet.',
     66988: 'Si tu as terminé [Mawdrey](https://lebusmagique.fr/pages/mises-a-jour-passees/col-aride/objet-de-dos-maudrey-et-maudrey-ii.html), tu peux jeter cet objet.',
 
     70026: 'Détruis-le ou garde-le pour le jeter à la Forge mystique.',
+    70229: 'Utilise [GW2Efficiency](https://gw2efficiency.com/account/dyes?filter.birthday=Celebratory%20Dye%20Pack) pour t\'aider à choisir une teinture (liée au compte).',
 
     73163: 'À recycler si ses statistiques ne t\'intéressent pas...',
 
     74268: 'À utiliser à la Forge Mystique pour terminer la collection [Gros Tom](https://www.lebusmagique.fr/pages/succes/les-collections/collections-basiques/collection-basique-gros-tom.html).',
 
+    75036: 'Si cet objet est bien enregistré dans ta collection "Incinératrice I&nbsp;: la dague expérimentale", tu peux le vendre à un marchand.',
+
+    79351: 'Utilise [GW2Efficiency](https://gw2efficiency.com/account/dyes?filter.birthday=Jubilant%20Dye%20Pack) pour t\'aider à choisir une teinture (liée au compte).',
+
     82518: 'Consomme la lettre pour obtenir 1&nbsp;000 points de karma.',
+    82589: 'Utilise [GW2Efficiency](https://gw2efficiency.com/account/dyes?filter.birthday=Exuberant%20Dye%20Kit) pour t\'aider à choisir une teinture (liée au compte).',
+
+    83008: 'À ouvrir puis recycler le contenu obtenu.',
 
     84335: 'Utile pour la [Chasse aux trésors enfouis](https://www.lebusmagique.fr/pages/gw2-pof/hautes-terres-du-desert/metapof-tresor-enfoui.html).',
+    84731: 'À ouvrir puis recycler le contenu obtenu.',
+
+    85016: 'À ouvrir puis recycler le contenu obtenu.',
 
     87517: 'Consomme ces clés en réalisant le succès [Maître de la plongée](https://lebusmagique.fr/pages/succes/general/chasseur-de-tresor-englouti.html) (10 clés). Quelques pièces d\'or à la... clé&nbsp;! 😉',
 
     89140: 'Transforme-les en [Tas de cristaux luisants](https://wiki-fr.guildwars2.com/wiki/Tas_de_cristaux_luisants) pour libérer de la place (10 particules =&nbsp;1&nbsp;tas).',
 
     92209: 'Utilise-le sur un personnage que tu joues beaucoup pour libérer un emplacement d\'inventaire.',
+    92317: 'Utile entre autres pour fabriquer les [armes boréales restaurées](https://www.lebusmagique.fr/pages/gw2-pof/l-epopee-du-givre/frontiere-de-bjora/murmure-dans-la-nuit/maitre-de-la-forge-ancestrale.html) puis [illuminées](https://www.lebusmagique.fr/pages/succes/les-collections/collections-rares/crpof-armes-boreales-illuminees.html) ou des [sacs de 20 à 32 emplacements](https://www.lebusmagique.fr/pages/gw2-pof/l-epopee-du-givre/sac-32-emplacements-epopee-du-givre.html).',
     92948: 'Tu peux le détruire.'
 }
