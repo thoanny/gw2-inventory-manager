021af90a184b2d1992bc4aaa528c09e042600f5b
Anthony Destenay <anthony.destenay@gmail.com>
2022-05-14 15:16:37 +0200

    Tips for 24476, 24517, 24523, 37347, 37644, 42052, 42053, 43740, 67979, 71100, 71126, 72620, 73025, 73089, 73506, 73640, 74535, 74560, 75742, 75919, 76712, 77507, 78134, 79328, 79337, 79696, 83315, 83664, 84077, 84286, 88239, 88247, 88261, 88277, 88307, 88344, 88349, 90001, 96717, 97116

--- tips.js
+++ tips.js
@@ -3,9 +3,19 @@ let tips = {
 
     20030: 'Tu peux détruire cet objet. Il se rachète à Sarettokk à l\'Arche du Lion.',
 
+    24476: 'À vendre au comptoir ou à un marchand, en fonction des prix, des offres et des demandes.',
+    24517: 'À vendre au comptoir ou à un marchand, en fonction des prix, des offres et des demandes.',
+    24523: 'À vendre au comptoir ou à un marchand, en fonction des prix, des offres et des demandes.',
     24531: 'À vendre au comptoir ou à un marchand, en fonction des prix, des offres et des demandes.',
 
-    37087: 'À infuser et/ou passer en affinité en fonction de tes ressources, avant de recycler si les stats ne t\'intéressent pas.',
+    37087: 'À [infuser et/ou passer en affinité](https://golemjoyeux.com/mettre-en-affinite-et-infuser-un-objet/) en fonction de tes ressources, avant de recycler si les stats ne t\'intéressent pas.',
+    37347: 'À recycler.',
+    37644: 'À recycler.',
+
+    42052: 'À recycler pour recevoir 5 insignes d\'honneur, si tu n\'as pas besoin de ses stats.',
+    42053: 'À recycler pour recevoir 5 insignes d\'honneur, si tu n\'as pas besoin de ses stats.',
+
+    43740: 'À utiliser sur un cor de guerre pour lui appliquer cette apparence ou à jeter pour faire de la place.',
 
     45001: 'Si tu as déjà débloqué cette miniature, tu peux la détruire.',
 
@@ -29,22 +39,50 @@ let tips = {
     66992: 'Si tu as terminé [Mawdrey](https://lebusmagique.fr/pages/mises-a-jour-passees/col-aride/objet-de-dos-maudrey-et-maudrey-ii.html), tu peux jeter cet objet.',
     66988: 'Si tu as terminé [Mawdrey](https://lebusmagique.fr/pages/mises-a-jour-passees/col-aride/objet-de-dos-maudrey-et-maudrey-ii.html), tu peux jeter cet objet.',
 
+    67979: 'Clé du coffre au centre du labyrinthe ouvert après que la rageliane ait été tuée.',
+
     70026: 'Détruis-le ou garde-le pour le jeter à la Forge mystique.',
     70229: 'Utilise [GW2Efficiency](https://gw2efficiency.com/account/dyes?filter.birthday=Celebratory%20Dye%20Pack) pour t\'aider à choisir une teinture (liée au compte).',
 
-    73163: 'À recycler si ses statistiques ne t\'intéressent pas...',
+    71100: 'Si cet objet est bien enregistré dans ta collection "Kudzu III : Feuille de Kudzu", tu peux le jeter.',
+    71126: 'Une fois enregistré dans la collection du succès [Rodgort II : La torche perfectionnée](https://www.lebusmagique.fr/pages/succes/les-collections/armes-legendaires/rodgort-ii-la-torche-perfectionnee.html), tu peux le vendre à un marchand.',
 
+    72620: 'Une fois enregistrée dans la collection du succès [Rodgort II : La torche perfectionnée](https://www.lebusmagique.fr/pages/succes/les-collections/armes-legendaires/rodgort-ii-la-torche-perfectionnee.html), tu peux le vendre à un marchand.',
+
+    73025: 'Si cet objet est bien enregistré dans ta collection "Kudzu II : L\'arc long perfectionné", tu peux le jeter.',
+    73089: 'Si cet objet est bien enregistré dans ta collection "Kudzu II : L\'arc long perfectionné", tu peux le vendre à un marchand.',
+    73163: 'À recycler si ses statistiques ne t\'intéressent pas...',
+    73506: 'Déverouille l\'apparence si tu ne l\'as pas déjà pour le succès des [Armes automatisées](https://www.lebusmagique.fr/pages/succes/les-collections/collections-rares/collection-rare-armes-automatisees.html), garde-le pour la forge mystique ou recycle-le.',
+    73640: 'Si cet objet est bien enregistré dans ta collection "Kudzu II : L\'arc long perfectionné", tu peux le vendre à un marchand.',
+
+    74535: 'Déverouille l\'apparence si tu ne l\'as pas déjà pour le succès des [Armes automatisées](https://www.lebusmagique.fr/pages/succes/les-collections/collections-rares/collection-rare-armes-automatisees.html), garde-le pour la forge mystique ou recycle-le.',
     74268: 'À utiliser à la Forge Mystique pour terminer la collection [Gros Tom](https://www.lebusmagique.fr/pages/succes/les-collections/collections-basiques/collection-basique-gros-tom.html).',
+    74560: 'Si cet objet est bien enregistré dans ta collection "Kudzu II : L\'arc long perfectionné", tu peux le vendre à un marchand.',
 
     75036: 'Si cet objet est bien enregistré dans ta collection "Incinératrice I&nbsp;: la dague expérimentale", tu peux le vendre à un marchand.',
+    75742: 'Une fois enregistré dans la collection du succès [Espoir IV : Le Catalyseur](https://www.lebusmagique.fr/pages/succes/les-collections/armes-legendaires/espoir-iv-le-catalyseur.html), tu peux le jeter.',
+    75919: 'Achète jusqu\'à 30 [clés à prix vraiment cassé](https://wiki-fr.guildwars2.com/wiki/Cl%C3%A9_d%27encodage_fractal_%C3%A0_prix_vraiment_cass%C3%A9) par jour pour ouvrir ces boîtes, dans le hall des fractales au marchand ACH-4373.',
 
+    76712: 'Avec 10 écrits, tu peux ouvrir le [coffre de Zinn](https://wiki.guildwars2.com/wiki/Zinn%27s_Research_Supplies) à Rata Novus.',
+
+    77507: 'À garder pour créer le précurseur de l\'objet de dos du JcJ.',
+
+    78134: 'Une fois enregistré dans la collection du succès [Chuka et Champawat II : L\'Embuscade](https://www.lebusmagique.fr/pages/succes/les-collections/armes-legendaires/chuka-et-champawat-ii-l-embuscade.html), tu peux le jeter.',
+
+    79328: 'À utiliser sur un objet de dos pour lui appliquer cette apparence ou à jeter pour faire de la place.',
+    79337: 'À utiliser sur un objet de dos pour lui appliquer cette apparence ou à jeter pour faire de la place.',
     79351: 'Utilise [GW2Efficiency](https://gw2efficiency.com/account/dyes?filter.birthday=Jubilant%20Dye%20Pack) pour t\'aider à choisir une teinture (liée au compte).',
+    79696: 'Garde-le si tu as peu d\'apparences débloquées sur ton compte, pour maximimer tes chances d\'obtenir des apparences exclusives.',
 
     82518: 'Consomme la lettre pour obtenir 1&nbsp;000 points de karma.',
     82589: 'Utilise [GW2Efficiency](https://gw2efficiency.com/account/dyes?filter.birthday=Exuberant%20Dye%20Kit) pour t\'aider à choisir une teinture (liée au compte).',
 
     83008: 'À ouvrir puis recycler le contenu obtenu.',
+    83315: 'À utiliser sur une dague pour lui appliquer cette apparence ou à jeter pour faire de la place.',
+    83664:  'À utiliser sur un espadon pour lui appliquer cette apparence ou à jeter pour faire de la place.',
 
+    84077: 'À utiliser sur un bâton pour lui appliquer cette apparence ou à jeter pour faire de la place.',
+    84286: 'À utiliser sur une hache pour lui appliquer cette apparence ou à jeter pour faire de la place.',
     84335: 'Utile pour la [Chasse aux trésors enfouis](https://www.lebusmagique.fr/pages/gw2-pof/hautes-terres-du-desert/metapof-tresor-enfoui.html).',
     84731: 'À ouvrir puis recycler le contenu obtenu.',
 
@@ -52,9 +90,23 @@ let tips = {
 
     87517: 'Consomme ces clés en réalisant le succès [Maître de la plongée](https://lebusmagique.fr/pages/succes/general/chasseur-de-tresor-englouti.html) (10 clés). Quelques pièces d\'or à la... clé&nbsp;! 😉',
 
+    88239: 'À utiliser sur des épaulières lourdes pour leurs appliquer cette apparence ou à jeter pour faire de la place.',
+    88247: 'À utiliser sur un torse lourd pour lui appliquer cette apparence ou à jeter pour faire de la place.',
+    88261: 'À utiliser sur un couvre-chef lourd pour lui appliquer cette apparence ou à jeter pour faire de la place.',
+    88277: 'À utiliser sur des gants lourds pour leurs appliquer cette apparence ou à jeter pour faire de la place.',
+    88307: 'Utilise [GW2Efficiency](https://gw2efficiency.com/account/dyes?filter.birthday=Triumphant%20Dye%20Kit) pour t\'aider à choisir une teinture (liée au compte).',
+    88344: 'À utiliser sur des bottes lourdes pour leurs appliquer cette apparence ou à jeter pour faire de la place.',
+    88349: 'À utiliser sur des jambières lourdes pour leurs appliquer cette apparence ou à jeter pour faire de la place.',
+
     89140: 'Transforme-les en [Tas de cristaux luisants](https://wiki-fr.guildwars2.com/wiki/Tas_de_cristaux_luisants) pour libérer de la place (10 particules =&nbsp;1&nbsp;tas).',
+
+    90001: 'Utile pour créer la [mini-lanterne scintillante](https://wiki-fr.guildwars2.com/wiki/Mini-lanterne_scintillante), sinon à vendre au comptoir.',
 
     92209: 'Utilise-le sur un personnage que tu joues beaucoup pour libérer un emplacement d\'inventaire.',
     92317: 'Utile entre autres pour fabriquer les [armes boréales restaurées](https://www.lebusmagique.fr/pages/gw2-pof/l-epopee-du-givre/frontiere-de-bjora/murmure-dans-la-nuit/maitre-de-la-forge-ancestrale.html) puis [illuminées](https://www.lebusmagique.fr/pages/succes/les-collections/collections-rares/crpof-armes-boreales-illuminees.html) ou des [sacs de 20 à 32 emplacements](https://www.lebusmagique.fr/pages/gw2-pof/l-epopee-du-givre/sac-32-emplacements-epopee-du-givre.html).',
-    92948: 'Tu peux le détruire.'
+    92948: 'Tu peux le détruire.',
+
+    96717: 'À utiliser si les stats t\'intéressent (chamanique, officinal ou ritualiste) ou à recycler.',
+
+    97116: 'À utiliser si les stats t\'intéressent (draconique, enragé ou nécrophage) ou à recycler.',
 }
